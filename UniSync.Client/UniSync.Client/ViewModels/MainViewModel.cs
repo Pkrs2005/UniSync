1ef8ef3bb7005faa6fd1d38480e5f3aab459289e
Tony <pkrs2005@mail.ru>
2026-06-08 18:28:18 +0300

    Create application

---
+++ UniSync.Client/UniSync.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UniSync.Client.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _greeting = "Welcome to Avalonia!";
+}
---
+++ UniSync.Client/UniSync.Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UniSync.Client.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
